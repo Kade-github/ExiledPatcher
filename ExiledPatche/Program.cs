93e4d88475f0abe224d29b052d1158ef1e45431a
Kade <KadePcGames@gmail.com>
2020-02-25 16:47:18 -0800

    Project Files

---
+++ ExiledPatche/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+
+namespace ExiledPatche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    Console.WriteLine("Missing file location argument!");
+                    return;
+                }
+
+                var Module = ModuleDefMD.Load(args[0]);
+
+                if (Module == null)
+                {
+                    Console.WriteLine("File not found!");
+                    return;
+                }
+
+                Module.IsILOnly = true;
+                Module.VTableFixups = null;
+                Module.IsStrongNameSigned = false;
+                Module.Assembly.PublicKey = null;
+                Module.Assembly.HasPublicKey = false;
+
+                var opts = new ModuleWriterOptions(Module);
+
+                Console.WriteLine("[EXILED] Loaded " + Module.Name);
+
+                Console.WriteLine("[EXILED-ASSEMBLY] Resolving References...");
+
+                ModuleContext modCtx = ModuleDef.CreateModuleContext();
+                // It creates the default assembly resolver
+                AssemblyResolver asmResolver = (AssemblyResolver) modCtx.AssemblyResolver;
+
+
+
+                Module.Context = modCtx;
+
+                ((AssemblyResolver) Module.Context.AssemblyResolver).AddToCache(Module);
+
+                Console.WriteLine("[INJECTION] Injecting the ModLoader Class.");
+
+                var ModLoader = ModuleDefMD.Load("ModLoader.dll");
+
+                Console.WriteLine("[INJECTION] Loaded " + ModLoader.Name);
+
+                var ModClass = ModLoader.Types[0];
+
+                foreach (var type in ModLoader.Types)
+                {
+                    if (type.Name == "ModLoader")
+                    {
+                        ModClass = type;
+                        Console.WriteLine("[INJECTION] Hooked to: " + type.Namespace + "." + type.Name);
+                    }
+                }
+
+                var modRefType = ModClass;
+
+
+                ModLoader.Types.Remove(ModClass);
+
+                modRefType.DeclaringType = null;
+
+                Module.Types.Add(modRefType);
+
+                MethodDef call = findMethod(modRefType, "LoadBoi");
+
+                if (call == null)
+                {
+                    Console.WriteLine("Failed to get the 'LoadBoi' method! Maybe we don't have permission?");
+                    return;
+                }
+
+                Console.WriteLine("[INJECTION] Injected!");
+
+                Console.WriteLine("[EXILED] Completed injection!");
+
+                Console.WriteLine("[EXILED] Patching code...");
+
+                TypeDef def = findType(Module.Assembly, "ServerConsoleSender");
+
+                MethodDef bctor = new MethodDefUser(".ctor", MethodSig.CreateInstance(Module.CorLibTypes.Void),
+                    MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                    MethodAttributes.Public |
+                    MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+
+                if (findMethod(def, ".ctor") != null)
+                {
+                    bctor = findMethod(def, ".ctor");
+                    Console.WriteLine("[EXILED] Re-using constructor.");
+                }
+                else
+                    def.Methods.Add(bctor);
+
+                CilBody body;
+                bctor.Body = body = new CilBody();
+                
+                body.Instructions.Add(OpCodes.Call.ToInstruction(call));
+                body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+                Module.Write("Assembly-CSharp-EXILED.dll");
+                Console.WriteLine("[EXILED] COMPLETE!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.Read();
+        }
+
+        private static MethodDef findMethod(TypeDef type, string methodName)
+        {
+            if (type != null)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (method.Name == methodName)
+                        return method;
+                }
+            }
+            return null;
+        }
+
+        private static MethodDef findMethod(AssemblyDef asm, string classPath, string methodName)
+        {
+            return findMethod(findType(asm, classPath), methodName);
+        }
+
+        private static TypeDef findType(AssemblyDef asm, string classPath)
+        {
+            foreach (var module in asm.Modules)
+            {
+                foreach (var type in module.Types)
+                {
+                    if (type.FullName == classPath)
+                        return type;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ModLoader/ModLoader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Loader
+{
+	public class ModLoader
+	{
+		// Token: 0x06002341 RID: 9025 RVA: 0x00002FD1 File Offset: 0x000011D1
+		public static void InitMods()
+		{
+		}
+
+		// Token: 0x06002342 RID: 9026 RVA: 0x000B10B4 File Offset: 0x000AF2B4
+		public static byte[] ReadFile(string path)
+		{
+			FileStream fileStream = File.Open(path, FileMode.Open);
+			byte[] result;
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				fileStream.CopyTo(memoryStream);
+				result = memoryStream.ToArray();
+			}
+			fileStream.Close();
+			return result;
+		}
+
+		// Token: 0x06002343 RID: 9027 RVA: 0x00020440 File Offset: 0x0001E640
+		public static void LoadBoi()
+		{
+			ModLoader.Loadxd();
+		}
+
+		// Token: 0x06002344 RID: 9028 RVA: 0x000B1100 File Offset: 0x000AF300
+		public static void Loadxd()
+		{
+			if (ModLoader.loaded)
+			{
+				return;
+			}
+			ServerConsole.AddLog("Hello, yes, EXILED is loading..");
+			try
+			{
+				ModLoader.loaded = true;
+				string text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EXILED");
+				if (!Directory.Exists(text))
+				{
+					Directory.CreateDirectory(text);
+				}
+				if (File.Exists(Path.Combine(text, "EXILED.dll")))
+				{
+					byte[] rawAssembly = ModLoader.ReadFile(Path.Combine(text, "EXILED.dll"));
+					try
+					{
+						MethodInfo methodInfo = Assembly.Load(rawAssembly).GetTypes().SelectMany((Type p) => p.GetMethods()).FirstOrDefault((MethodInfo f) => f.Name == "EntryPointForLoader");
+						if (methodInfo != null)
+						{
+							methodInfo.Invoke(null, null);
+						}
+					}
+					catch (Exception arg)
+					{
+						ServerConsole.AddLog(string.Format("EXILED load error: {0}", arg));
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				ServerConsole.AddLog(ex.ToString());
+			}
+		}
+
+		// Token: 0x04001FEB RID: 8171
+		private static bool loaded;
+	}
+}
